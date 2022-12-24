80af29ca48c168bae0d3765e900b373e65c52ff7
crissnb <crissnb472@gmail.com>
2022-12-24 13:26:53 +0100

    cleaned repo

--- sketchybar/UpdateDynamicIslandRepo.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-cp -r "$HOME/.config/sketchybar/plugins/dynamic_island/." "$HOME/Developer/Other/Dynamic-Island-Sketchybar/plugins/dynamic_island/."
-cp "$HOME/.config/sketchybar/items/dynamic_island.sh" "$HOME/Developer/Other/Dynamic-Island-Sketchybar/items/dynamic_island.sh"
